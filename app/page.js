1c40c63f6e611f440ad9abd5c84843ce4d7039f6
hannesvienna9000 <feistenauer@gmail.com>
2023-07-14 11:07:49 +0200

    Install and test tailwindcss

--- app/page.js
+++ app/page.js
@@ -3,19 +3,24 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './globals.css';
+
 
 export default function Home() {
 
-  const [message, setMessage] = useState('');
+//  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await axios.get('http://localhost:5328/api/hello');
-      setMessage(response.data); 
-    }
-    fetchData();
-  }, []);
+//useEffect(() => {
+//    async function fetchData() {
+//      const response = await axios.get('http://localhost:5328/api/hello');
+//      setMessage(response.data); 
+//    }
+//    fetchData();
+//  }, []);
 
-  return <div>{message}</div>;
-
+return (
+  <div className="p-4 max-w-md mx-auto bg-indigo-200 rounded-lg shadow-md">
+    <h1 className="text-lg font-medium text-gray-700">Hello Tailwindcss</h1>
+  </div>
+);
 }

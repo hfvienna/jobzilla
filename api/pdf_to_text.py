7cf832bd380fd5098650b3d7c32a9e14edac86f2
hannesvienna9000 <feistenauer@gmail.com>
2023-07-15 15:11:43 +0200

    Start process for PDFs only once

--- api/pdf_to_text.py
+++ api/pdf_to_text.py
@@ -11,18 +11,33 @@ INPUT_FOLDER = os.path.join(ROOT_DIR, "public/jobs/pdfs")
 OUTPUT_FOLDER = os.path.join(ROOT_DIR, "public/jobs/txts")
 
 
-def extract_pdf_text():
-    input_pdf_paths = list(Path(INPUT_FOLDER).glob("*.pdf"))
-
-    if not input_pdf_paths:
-        logging.error(f"No PDFs found in input folder {INPUT_FOLDER}")
+def get_processed_files(log_file):
+    if os.path.exists(log_file):
+        with open(log_file, 'r') as f:
+            return f.read().splitlines()
     else:
-        logging.info(f"{len(input_pdf_paths)} PDFs found in input folder")
+        return []
+
+def log_processed_file(log_file, file_name):
+    with open(log_file, 'a') as f:
+        f.write(file_name + '\n')
+
+def extract_pdf_text():
+    log_file = 'processed_files.log'
+    processed_files = get_processed_files(log_file)
+    
+    input_pdf_paths = list(Path(INPUT_FOLDER).glob("*.pdf"))
+    unprocessed_pdf_paths = [path for path in input_pdf_paths if path.name not in processed_files]
+
+    if not unprocessed_pdf_paths:
+        logging.error(f"No unprocessed PDFs found in input folder {INPUT_FOLDER}")
+    else:
+        logging.info(f"{len(unprocessed_pdf_paths)} unprocessed PDFs found in input folder")
 
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
 
-    for pdf_path in input_pdf_paths:
+    for pdf_path in unprocessed_pdf_paths:
         try:
             pdf = PdfReader(str(pdf_path))
         except Exception as e:
@@ -44,6 +59,8 @@ def extract_pdf_text():
                 logging.info(f"Text extracted to {output_path}")
         except Exception as e:
             logging.exception(f"Error writing to {output_path}: {e}")
+        else:
+            log_processed_file(log_file, pdf_path.name)
 
     logging.info("PDF extraction complete")
 

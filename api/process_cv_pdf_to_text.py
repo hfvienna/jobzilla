4a658d3e298e8a5e618c294dca8892e7675a435c
hannesvienna9000 <feistenauer@gmail.com>
2023-07-18 15:48:45 +0200

    Add CV

---
+++ api/process_cv_pdf_to_text.py
@@ -0,0 +1,23 @@
+"""
+Take pdf from folder public/digitaltwin/cv-hfvienna.pdf and extract text to txt in same folder
+Save it as cv_hfvienna.txt
+"""
+
+import os
+from PyPDF2 import PdfReader
+
+def convert_pdf_to_txt(pdf_path, txt_path):
+    with open(pdf_path, 'rb') as pdf_file:
+        reader = PdfReader(pdf_file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+    
+    with open(txt_path, 'w') as txt_file:
+        txt_file.write(text)
+
+if __name__ == "__main__":
+    pdf_path = os.path.join('..', 'public', 'digitaltwin', 'cv_hfvienna.pdf')
+    txt_path = os.path.join('..', 'public', 'digitaltwin', 'cv_hfvienna.txt')
+    convert_pdf_to_txt(pdf_path, txt_path)
+    

54d5c043067768b7f0bdd45aeb64bc2b0b2ca6f2
hannesvienna9000 <feistenauer@gmail.com>
2023-07-18 23:52:59 +0200

    Add truncate view

--- app/ranking/page.js
+++ app/ranking/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import "daisyui/dist/full.css";
 
@@ -9,13 +8,21 @@ import tableData from "../job_table.json";
 // Define fallback value
 const DEFAULT_VALUE = "-";
 
+// Truncate text to maximum length
+const truncate = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + '...';
+  }
+  return text;
+}
+
 export default function JobsTable() {
   const [jobs] = useState(tableData);
 
   useEffect(() => {}, []);
 
   return (
-    <table className="table table-zebra w-full">
+    <table className="table table-zebra">
       <thead>
         <tr>
           <th>UUID</th>
@@ -23,7 +30,7 @@ export default function JobsTable() {
           <th>Job Title</th>
           <th>Fit Total</th>
           <th>Fit Recruiter View</th>
-          <th>Fit Recruiter View Detailed</th> 
+          <th>Fit Recruiter View Detailed</th>
           <th>Fit Applicant View</th>
           <th>Fit Applicant View Detailed</th>
           <th>Date Added</th>
@@ -31,7 +38,6 @@ export default function JobsTable() {
           <th>Location</th>
         </tr>
       </thead>
-
       <tbody>
         {jobs.map((job, index) => (
           <tr key={job.id}>
@@ -40,16 +46,15 @@ export default function JobsTable() {
             <td>{job.title || DEFAULT_VALUE}</td>
             <td>{(job.fit_recruiter + job.fit_applicant) || DEFAULT_VALUE}</td>
             <td>{job.fit_recruiter || DEFAULT_VALUE}</td>
-            <td>{job.fit_recruiter_detailed || DEFAULT_VALUE}</td>
+            <td>{truncate(job.fit_recruiter_detailed, 200) || DEFAULT_VALUE}</td>
             <td>{job.fit_applicant || DEFAULT_VALUE}</td>
-            <td>{job.fit_applicant_detailed || DEFAULT_VALUE}</td>
+            <td>{truncate(job.fit_applicant_detailed, 200) || DEFAULT_VALUE}</td>
             <td>{job.date_added || DEFAULT_VALUE}</td>
-            <td>{job.salary_Range || DEFAULT_VALUE}</td>
+            <td>{job.salary_range || DEFAULT_VALUE}</td>
             <td>{job.location || DEFAULT_VALUE}</td>
           </tr>
         ))}
       </tbody>
-
       <tfoot>
         <tr>
           <th>UUID</th>
@@ -57,7 +62,7 @@ export default function JobsTable() {
           <th>Job Title</th>
           <th>Fit Total</th>
           <th>Fit Recruiter View</th>
-          <th>Fit Recruiter View Detailed</th> 
+          <th>Fit Recruiter View Detailed</th>
           <th>Fit Applicant View</th>
           <th>Fit Applicant View Detailed</th>
           <th>Date Added</th>

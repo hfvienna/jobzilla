ad4a701f66b6036db4974526da37517fb81fbb88
hannesvienna9000 <feistenauer@gmail.com>
2023-07-23 17:33:23 +0200

    Bugfix main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ src_dir = os.path.dirname(os.path.abspath(__file__))
 # List of Python files in the order they should be executed
 py_files = [
     'rename_job_posting_pdf_to_add_uuid.py',
-    'convert_job_posting_pdf_to_txt.py'
+    'convert_job_posting_pdf_to_txt.py',
     'extract_job_posting_facts_to_json.py',
     'evaluate_job_fit_from_recruiter_view.py',
     'evaluate_job_fit_from_applicant_view.py',
@@ -17,8 +17,14 @@ py_files = [
 ]
 
 # Run each Python file
+# Run each Python file
 for py_file in py_files:
     py_file_path = os.path.join(src_dir, py_file)
     if os.path.isfile(py_file_path):
         print(f'Running {py_file_path}...')
-        subprocess.run(['python3', py_file_path], check=True)
+        try:
+            subprocess.run(['python3', py_file_path], check=True)
+        except subprocess.CalledProcessError:
+            print(f"Error occurred while running {py_file_path}")
+    else:
+        print(f"File {py_file_path} does not exist")

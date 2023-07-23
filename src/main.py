e75182361ba7e8823516a3edbc4258eef50a69b6
hannesvienna9000 <feistenauer@gmail.com>
2023-07-23 17:17:06 +0200

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+# Directory of the currently running script
+src_dir = os.path.dirname(os.path.abspath(__file__))
+
+# List of Python files in the order they should be executed
+py_files = [
+    'rename_job_posting_pdf_to_add_uuid.py',
+    'convert_job_posting_pdf_to_txt.py'
+    'extract_job_posting_facts_to_json.py',
+    'evaluate_job_fit_from_recruiter_view.py',
+    'evaluate_job_fit_from_applicant_view.py',
+    'merge_applicant_and_recruiter_views.py',
+    'aggregate_jsons_to_table_to_display_ranked_jobs.py',
+    'sanity_check.py'
+]
+
+# Run each Python file
+for py_file in py_files:
+    py_file_path = os.path.join(src_dir, py_file)
+    if os.path.isfile(py_file_path):
+        print(f'Running {py_file_path}...')
+        subprocess.run(['python3', py_file_path], check=True)

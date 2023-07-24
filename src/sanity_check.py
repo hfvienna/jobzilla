9eca2f1e815c52b39e5fd66215abb6575b771bb7
hannesvienna9000 <feistenauer@gmail.com>
2023-07-24 11:34:34 +0200

    Add minimum score for application letter

--- src/sanity_check.py
+++ src/sanity_check.py
@@ -1,53 +1,22 @@
 import os
 import json
-import jsonschema
-from jsonschema import validate
-
-# Define the JSON schema you expect
-schema = {
-    "type" : "object",
-    "properties" : {
-        "company" : {"type" : "string"},
-        "title" : {"type" : "string"},
-        "fit" : {"type" : ["integer", "null"]},
-        "fit_detailed" : {"type" : "string"},
-        "dateAdded" : {"type" : "string"},
-        "salaryRange" : {"type" : "string"},
-        "location" : {"type" : "string"},
-        "email" : {"type" : "string"},
-    },
-}
 
 def count_files(directory):
     num_files = 0
-#    num_valid_json = 0
-#    num_valid_schema = 0
-#    invalid_json_files = []
-#    invalid_schema_files = []
+    num_high_fit = 0
     for f in os.listdir(directory):
         if f.startswith('.') or f.endswith('.DS_Store'):  # Skip hidden files and .DS_Store files
             continue
         if os.path.isfile(os.path.join(directory, f)):
             num_files += 1
-#           if f.endswith('.json'):
-#                with open(os.path.join(directory, f)) as json_file:
-#                    try:
-#                        data = json.load(json_file)
-#                        num_valid_json += 1
-#                        # Validate the loaded JSON against the schema
-#                        validate(instance=data, schema=schema)
-#                        num_valid_schema += 1
-#                        # If the 'fit_detailed' key contains "", print the JSON
-#                        if directory == "../public/jobs/JSONs_fits" and '""' in data.get('fit_detailed', ''):
-#                            print(f"JSON from file {f} with '\"\"' in 'fit_detailed': {data}")
-#                    except json.JSONDecodeError:
-#                        print(f"Invalid JSON file: {f}")
-#                        invalid_json_files.append(f)
-#                    except jsonschema.exceptions.ValidationError as ve:
-#                        print(f"JSON file does not match schema: {f}, {ve}")
-#                        invalid_schema_files.append(f)
-
-    return num_files #, num_valid_json, num_valid_schema, invalid_json_files, invalid_schema_files
+            if f.endswith('.json'):
+                with open(os.path.join(directory, f)) as json_file:
+                    data = json.load(json_file)
+                    # Calculate the total fit
+                    total_fit = (data.get('fit_applicant', 0) or 0) + (data.get('fit_recruiter', 0) or 0)
+                    if total_fit >= 80:
+                        num_high_fit += 1
+    return num_files, num_high_fit
 
 directories = [
     "../data/jobs/job_postings_inbox_pdf",
@@ -57,12 +26,11 @@ directories = [
     "../data/jobs/fits_recruiter_json",
     "../data/jobs/fits_applicant_json",
     "../data/jobs/fits_merged_json",
+    "../data/jobs/application_letters_txt"
 ]
 
 for directory in directories:
-    num_files = count_files(directory) # , num_valid_json, num_valid_schema, invalid_json_files, invalid_schema_files
+    num_files, num_high_fit = count_files(directory)
     print(f"There are {num_files} files in {directory}")
-#    print(f"There are {num_valid_json} valid JSON files in {directory}")
-#    print(f"There are {num_valid_schema} valid JSON files that match the schema in {directory}")
-#    print(f"Invalid JSON files: {invalid_json_files}")
-#    print(f"JSON files that do not match the schema: {invalid_schema_files}")
+    if directory.endswith("fits_merged_json"):
+        print(f"There are {num_high_fit} jobs with a fit score of 80 or higher in {directory}")
--- src/write_application_letter.py
+++ src/write_application_letter.py
@@ -5,17 +5,23 @@ import json
 from llm_claude import llm
 
 SYSTEM_MESSAGE = """
-Assume you are an expert job hunter.
+Assume you are an "expert job hunter".
 You have the CV from the applicant and the job posting.
 Write a complete application letter to send out to apply for the job.
 Only use arguments that you can prove from the CV and cite them with quotes to make it visible which source you cite.
 Write that your letter is automatically written using an LLM and a software that the applicant made.
-They can see the software under github.com/hfvienna/jobzilla and check out their companies ranking at jobzillaai.vercel.com
+They can see the software under https://github.com/hfvienna/jobzilla and check out their companies ranking at https://jobzillaai.vercel.app/ranking
+If the job posting is in German, write the application letter in German only.
+If the job posting is in English or any other language, write the application letter in English only.
+Sign as the name in the CV (assume you are the person in the CV plus having the skill of an expert job hunter).
+Write only things that belong in an application letter and convince the recruiter.
+Be concise, your application letter has to fit on one page including date etc.
 """
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 JOBS_FOLDER = os.path.join(ROOT_DIR, "../data/jobs/job_postings_renamed_txt")
 LETTERS_FOLDER = os.path.join(ROOT_DIR, "../data/jobs/application_letters_txt")
+SCORES_FOLDER = os.path.join(ROOT_DIR, "../data/jobs/fits_merged_json")
 CV_TXT = os.path.join(ROOT_DIR, "../data/digitaltwin/cv_hfvienna.txt")
 
 # Make sure the application_letters_txt directory exists
@@ -25,10 +31,32 @@ def load_file(path):
     with open(path, 'r') as f:
         return f.read()
 
+def load_json_file(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+
 for filename in os.listdir(JOBS_FOLDER):
+    if not filename.endswith(".txt"):  # Skip non-txt files
+        continue
+
     txt_filename = os.path.splitext(filename)[0] + ".txt"
     txt_path = os.path.join(JOBS_FOLDER, txt_filename)
-    
+    output_path = os.path.join(LETTERS_FOLDER, txt_filename)
+    score_path = os.path.join(SCORES_FOLDER, txt_filename.replace(".txt", ".json"))
+
+    # Check if the file already exists in the target folder
+    if os.path.exists(output_path):
+        logging.debug(f"Application letter for {txt_filename} already exists. Skipping.")
+        continue
+
+    # Check if the total score is less than 80
+    if os.path.exists(score_path):  # Make sure the score file exists
+        score_data = load_json_file(score_path)
+        total_score = score_data["fit_applicant"] + score_data["fit_recruiter"]
+        if total_score < 80:
+            logging.debug(f"Total score for {txt_filename} is less than 80. Skipping.")
+            continue
+
     # Load job text
     with open(txt_path) as f:
         job_text = f.read()
@@ -48,6 +76,6 @@ for filename in os.listdir(JOBS_FOLDER):
     logging.debug(f"Completion: {completion}")
 
     # Save result to txt file
-    output_path = os.path.join(LETTERS_FOLDER, txt_filename)
     with open(output_path, "w") as f:
         f.write(completion)
+        
